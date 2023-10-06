164b4269a8b7f9603431f014c416ff0b2e2fbabf
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-07 03:50:46 +0900

    ♻️ FooterとLinksをコンポーネント化

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Home.module.css'
+
+export function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <a
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Thanks by{' '}
+                <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+            </a>
+        </footer>
+    )
+}
