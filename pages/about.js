e3d64179e41d7268325be6ccd44de48bb03804c5
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-07 12:28:59 +0900

    🚑 正常にMainをimportするよう修正

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
-import { Main } from "next/document";
+import { Main } from "../components/Main";
 
 export default function Home() {
   return (
