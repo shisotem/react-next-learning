01471afbcf115540f6a32e1549e86a06655db172
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-08 12:11:00 +0900

    ✨ カウンタおよびuseStateによるカウントアップ機能の追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // const handleClick = (e, foo) => {
 //   console.log(e.target.href);
@@ -12,18 +12,16 @@ import { useCallback, useEffect } from "react";
 // };
 
 export default function Home() {
-  const foo = "Foo!";
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(1);
+
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
-    console.log("Mount!");
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("Unmount!");
       document.body.style.backgroundColor = "";
     };
   }, []);
@@ -34,9 +32,8 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
