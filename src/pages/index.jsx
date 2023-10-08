acd8446b8d0086b1641d587161cfc759da30b323
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-08 12:59:28 +0900

    ⚗️useCallbackで第2引数が指定されていない場合の挙動の実験
    この場合、useCallbackの再生成がなされないという特性上、countの値は1のままである。そのため、コンソール上では常に1が出力され、何度ボタンをクリックしてもsetCountが実行される。
    ※ただし、setCount((foo) => foo + 1);に関しては、前回の（const [count, setCount] = useState(1);の）countをみて処理をするという特性上、<h1>{count}</h1>で表示される数はクリック毎に無限にインクリメントされていく。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 // const handleClick = (e, foo) => {
 //   console.log(e.target.href);
@@ -14,19 +14,19 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback((e) => {
+    console.log(count);
+    if (count < 10) {
+      setCount((foo) => foo + 1);
+    }
+  }, []);
 
   useEffect(() => {
-    console.log(`マウント時: ${count}`);
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log(`アンマウント時: ${count}`);
       document.body.style.backgroundColor = "";
     };
-  }, [count]);
+  }, []);
 
   return (
     <div className={styles.container}>
