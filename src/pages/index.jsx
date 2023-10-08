4f97d464dd4de034bc3fd2583b957ab8a2ad6b68
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-08 11:42:10 +0900

    ✨ useEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 // const handleClick = (e, foo) => {
 //   console.log(e.target.href);
@@ -17,6 +17,15 @@ export default function Home() {
     console.log(e.target.href);
     e.preventDefault();
     alert(foo);
+  }, []);
+
+  useEffect(() => {
+    console.log("Mount!");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("Unmount!");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
