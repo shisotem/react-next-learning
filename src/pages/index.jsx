5d4332278147ff369dc3cf5405fa9d3fcf706d13
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-08 16:19:52 +0900

    ✨ useStateによる配列の処理を追加
    ・スプレッド構文
    オブジェクトや配列オブジェクトはmutableなので、破壊的操作（e.g. pop(), obj.prop = 1）を避け、immutableに扱う目的で、スプレッド構文が使われる。
    ※元の値を直接変更するのではなく、新しい値を返させる（→それを変数に束縛する）。
    ・Reactも破壊的操作を嫌う
    Reactコンポーネント内部で、
    setArr((prevArr) => {
      const newArr = prevArr;
      newArr.push(1);
      return newArr;
    });
    という記述があっても、
    newArr === prevArr // => true
    により、変化がないとみなされ、再レンダリングされない。
    ・スプレッド構文で配列を非破壊的に扱う
    一方、
    const newArr = prevArr;
    newArr.push(1);
    の部分を
    const newArr = [...prevArr, 1];
    とすれば、
    newArr === prevArr // => false
    により、再レンダリングが走る。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -28,6 +29,16 @@ export default function Home() {
     setText(e.target.value.trim());
   }, []);
 
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じテキストが既に存在します。");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
@@ -45,6 +56,12 @@ export default function Home() {
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
       <Footer />
     </div>
