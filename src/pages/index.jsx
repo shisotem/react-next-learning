9c75de11793058bb1a73c19c30516eb0b634b5b0
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-08 11:05:16 +0900

    ⚗️ クリックイベントの追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,31 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
+
+// const handleClick = (e, foo) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+//   alert(foo);
+// };
 
 export default function Home() {
+  const foo = "Foo!";
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
