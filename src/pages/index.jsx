17d3a1b2ef0930e9a2d216e0c0ed864e0f94e83f
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-08 16:18:18 +0900

    ♻️ useStateで扱う引数名を変更

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,12 +12,12 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount((foo) => foo + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   });
 
   const handleChange = useCallback((e) => {
